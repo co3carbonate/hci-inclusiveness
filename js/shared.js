4b62b09194716ce0ea36fb5ceec0f81969d15199
zhacatomn <zhacatomn@gmail.com>
2017-08-04 16:02:35 +0800

    added the nav bar

--- js/shared.js
+++ js/shared.js
@@ -1,2 +1,20 @@
 // All HTML files link to this JS file
+var counter = 1;
+function slideDown() {
+	$('.nav_').slideDown('slow');
+}
 
+function slideUp() {
+	$('.nav_').slideUp('slow');
+}
+
+$('.click').on('click', function(){
+	console.log("test");
+	if (counter) {
+		slideDown();
+		counter = 0;
+	} else {
+		slideUp();
+		counter = 1;
+	}
+});
